2b5e4fa5bf562ab145cc1eb5d1e7cb329c646e38
Nathan Turkson <nathanturkson@gmail.com>
2023-10-16 09:43:43 +0000

    mobile screen

--- src/components/ResultsSummary.jsx
+++ src/components/ResultsSummary.jsx
@@ -6,23 +6,23 @@ function ResultsSummary() {
       <div className="container text-center">
         <div className="row">
           <div className="col-12 col-md-6 result-section">
-            <h3> Your Result</h3>
-            <p>
+            <h3 className="result-header"> Your Result</h3>
+            <p className="score">
               <span>76 </span> of 100
             </p>
-            <h2>Great</h2>
-            <h3>
+            <h2 >Great</h2>
+            <h3 className="percentage">
               You scored higher than 65% of the people who have taken these
               tests.
             </h3>
           </div>
 
-          <div className="col-12 col-md-6">
+          <div className="col-12 col-md-6 summary-section">
             <h2>Summary</h2>
 
             <ul>
               <li>
-                <span> Reaction</span> <span> 80/100 </span>
+                <span>  Reaction</span> <span> 80/100 </span>
               </li>
 
               <li>
@@ -35,15 +35,13 @@ function ResultsSummary() {
 
               <li>
                 <span> Visual </span> <span> 72/100 </span>
-                          </li>
-                          
-                      </ul>
-                      
-                      <button>Continue</button>
+              </li>
+            </ul>
+
+            <button>Continue</button>
           </div>
         </div>
       </div>
-     
     </div>
   );
 }
